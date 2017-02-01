b1da6c2077aaf9168cef8fb8a293e9ca26a44804
Collin Brooks <cbrooks816@yahoo.com>
2017-02-01 02:06:46 -0600

    added Welcome page for assignment

--- main.py
+++ main.py
@@ -14,12 +14,107 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
+import cgi
+
+page_header = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Signup</title>
+        <style type="text/css">
+            .error {
+                color: red;
+            }
+        </style>
+    </head>"""
+
+
+page_body = """
+
+    <body>
+        <h1>
+            <a href = "/">Signup</a>
+        </h1>
+        <form action = "/welcome" method = "post">
+            <table>
+                <tr>
+                    <td><label for = "username">Username</label></td>
+                    <td>
+                        <input name="username" type="text" value="" required>
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="password">Password</label></td>
+                    <td>
+                        <input name="password" type="password" required>
+                        <span class ="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="verify">Verify Password</label></td>
+                    <td>
+                        <input name="verify" type="password" required>
+                        <span class ="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="email">Email (optional)</label></td>
+                    <td>
+                        <input name="email" type="email" value="">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>"""
+
+
+
+page_footer = """
+    </body>
+    </html>"""
 
 class MainHandler(webapp2.RequestHandler):
+
+
+
     def get(self):
-        self.response.write('Hello world!')
+
+        content = page_header + page_body + page_footer
+
+        self.response.write(content)
+
+class userNameError(webapp2.RequestHandler):
+
+    def post(self):
+
+
+        username = self.request.get("username")
+
+        
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+
+    def post(self):
+
+        username = self.request.get("username")
+        escaped_username = cgi.escape(username, quote = True )
+        new_username = "<strong>" + escaped_username + "</strong>"
+        welcome_sentence = "Welcome " + new_username + "!!"
+        content = page_header + welcome_sentence + page_footer
+
+        self.response.write(content)
+
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
+
 ], debug=True)
