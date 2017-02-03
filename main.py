3243e50a537d16958ccbed10b0da96299c40ff4a
Collin Brooks <cbrooks816@yahoo.com>
2017-02-02 20:47:37 -0600

    finished user signup assignment

--- main.py
+++ main.py
@@ -74,49 +74,49 @@ page_body = """
         </form>"""
 
 
+def page_body2(user_error1,user_error2,user_error3,user_error4):
 
+    page_body2 = ("""
 
-page_body2 = """
-
-        <body>
-            <h1>
-                <a href = "/">Signup</a>
-            </h1>
-            <form method = "post">
-                <table>
-                    <tr>
-                        <td><label for = "username">Username</label></td>
-                        <td>
-                            <input name="username" type="text" value="" >
-                            <span class="error">%(user_error1)s</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="password">Password</label></td>
-                        <td>
-                            <input name="password" type="password" required>
-                            <span class ="error">%(user_error2)s</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="verify">Verify Password</label></td>
-                        <td>
-                            <input name="verify" type="password" required>
-                            <span class ="error">%(user_error3)s</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="email">Email (optional)</label></td>
-                        <td>
-                            <input name="email" type="email" value="">
-                            <span class="error">%(user_error4)s</span>
-                        </td>
-                    </tr>
-                </table>
-                <input type="submit">
-            </form>"""%{"user_error1":"Not a valid username","user_error2":"Not a valid password",
-                        "user_error3":"Passwords do not match","user_error4":"Not a valid email"}
-
+            <body>
+                <h1>
+                    <a href = "/">Signup</a>
+                </h1>
+                <form method = "post">
+                    <table>
+                        <tr>
+                            <td><label for = "username">Username</label></td>
+                            <td>
+                                <input name="username" type="text" value="" >
+                                <span class="error">%(user_error1)s</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="password">Password</label></td>
+                            <td>
+                                <input name="password" type="password" required>
+                                <span class ="error">%(user_error2)s</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="verify">Verify Password</label></td>
+                            <td>
+                                <input name="verify" type="password" required>
+                                <span class ="error">%(user_error3)s</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="email">Email (optional)</label></td>
+                            <td>
+                                <input name="email" type="email" value="">
+                                <span class="error">%(user_error4)s</span>
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit">
+                </form>"""%{"user_error1":user_error1,"user_error2":user_error2,
+                            "user_error3":user_error3,"user_error4":user_error4})
+    return page_body2
 
 
 page_footer = """
@@ -161,19 +161,31 @@ class MainHandler(webapp2.RequestHandler):
 
 
         if not valid_username(username):
-
+            user_error1 = "Not a valid username"
             have_error = True
         else:
-            user_error = ""
+            user_error1 = ""
 
         if not valid_password(password):
+            user_error2 = "Not a valid password"
+            have_error = True
+        else:
+            user_error2 = ""
+
+        if password != verify:
+
+            user_error3 = "passwords do not match"
             have_error = True
 
-        elif password != verify:
-            have_error = True
+        else:
+            user_error3 = ""
 
         if not valid_email(email):
+            user_error4 = "not a valid email"
             have_error = True
+
+        else:
+            user_error4 = ""
 
 
 
@@ -182,7 +194,7 @@ class MainHandler(webapp2.RequestHandler):
             self.redirect('/welcome?username=' + username)
         else:
 
-            content2 = page_header + page_body2 + page_footer
+            content2 = page_header + page_body2(user_error1,user_error2,user_error3,user_error4) + page_footer
 
             self.response.write(content2)
 
